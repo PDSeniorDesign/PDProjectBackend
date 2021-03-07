9f28503ee31dfec1b697db421f3f9220a2c3fd3b
rtiare <ttorosa55@gmail.com>
2021-03-07 15:15:20 -0800

    Added admin controller

---
+++ src/main/java/sbrest/controller/AdminController.java
@@ -0,0 +1,111 @@
+package sbrest.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import sbrest.model.Admin;
+import sbrest.model.ServiceRequest;
+import sbrest.model.RequestStatusResponse;
+import sbrest.model.dao.AdminDao;
+import sbrest.model.dao.ServiceRequestDao;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+//These functions are for the admin user
+	
+	@Autowired
+    private AdminDao adminDao;
+	
+	@Autowired
+	private ServiceRequestDao serviceRequestDao;
+	
+	//Gets some details of all specified Service Request if user is admin
+	@GetMapping("/{password}/service_requests")
+	public List<RequestStatusResponse> getRequestStatusByRequestNumber(@PathVariable String password) {
+		List<ServiceRequest> serviceRequests = serviceRequestDao.getServiceRequests();
+		
+		List<RequestStatusResponse> requestStatusResponses = new ArrayList<RequestStatusResponse>();
+
+		// get password from user input and database, then compare
+		//If password matches one in database return request status, else 403 error forbidden
+		String dbPassword = adminDao.getAdmin().getPassword();
+
+		if (dbPassword.equals(password)) {
+			if (serviceRequests != null) {				
+				// pointer
+				for(ServiceRequest s : serviceRequests) {
+					RequestStatusResponse r = new RequestStatusResponse(s.getRequestNumber(),
+							s.getRequestStatus(), s.getFirstName(), s.getLastName());
+					requestStatusResponses.add(r);
+				}
+				return requestStatusResponses;
+			} else
+				////TODO: Null pointer exception if no service requests 
+				return null;
+		} else
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN,
+					"User does not have authorization to view this page");
+	}	
+	
+	//Gets all details of a specified Service Request if user is admin
+	@GetMapping("/{password}/service_requests/{requestNumber}")
+	public ServiceRequest get(@PathVariable String password, @PathVariable Integer requestNumber) {
+		ServiceRequest s = serviceRequestDao.getServiceRequest(requestNumber);
+
+		// get password from user input and database, then compare
+		//If password matches one in database return request status, else 403 error forbidden
+		String dbPassword = adminDao.getAdmin().getPassword();
+
+		if (dbPassword.equals(password)) {
+			if (s == null)
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Service Request not found");
+			return s;
+		} else {
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN,
+					"User does not have authorization to view this page");
+		}
+	}
+	
+	//Admin user can change the password
+	@PatchMapping("/reset_password/{newPassword}")
+	//@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void update(@RequestBody Admin oldAdmin, @PathVariable String newPassword) {
+		
+		// get password from user input and database, then compare
+		//If password matches one in database return request status, else 403 error forbidden
+		String oldPassword = oldAdmin.getPassword(); //user input
+		String dbPassword = adminDao.getAdmin().getPassword(); //database
+		
+		if (dbPassword.equals(oldPassword)) {
+			Admin originalAdmin = adminDao.getAdmin();
+			originalAdmin.setPassword(newPassword);
+			originalAdmin = adminDao.saveAdmin(originalAdmin);	
+		}else {
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN,
+					"User does not have authorization to view this page");
+		}
+	}
+	
+
+// NEEDED?
+//		@GetMapping
+//	    public List<Admin> Admin(ModelMap models) {
+//	        return adminDao.getAdmins();
+//	    }
+	
+}
