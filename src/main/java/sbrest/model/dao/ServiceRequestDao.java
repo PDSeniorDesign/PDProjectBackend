0875a076d1611a1393097bdca21dfe2d4f87843d
rtiare <ttorosa55@gmail.com>
2021-03-07 15:17:20 -0800

    Modified service request dao

--- src/main/java/sbrest/model/dao/ServiceRequestDao.java
+++ src/main/java/sbrest/model/dao/ServiceRequestDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 import sbrest.model.ServiceRequest;
 
 public interface ServiceRequestDao {
+	
 	ServiceRequest getServiceRequest(Integer requestNumber);
 
     List<ServiceRequest> getServiceRequests();
