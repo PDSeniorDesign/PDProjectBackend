5b69c7307dcd2824a051e532b11503f3942ca96f
Adrian Palomares <adrianpal45@gmail.com>
2021-04-05 14:36:23 -0700

    changed from controller to global cors

--- src/main/java/sbrest/DemoApplication.java
+++ src/main/java/sbrest/DemoApplication.java
@@ -2,7 +2,10 @@ package sbrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan
@@ -11,5 +14,18 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	
+	// Cors Configuration
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+				
+			}
+		};
+	}
+	
 
 }
